5dd6c0587331b31c4653fe08d3a0c17c6077ac22
nmhillusion <nguyenminhhieu.geek@gmail.com>
2025-05-29 22:28:05 +0700

    config for testing mode

--- src/main/java/tech/nmhillusion/jParrotDataSelectorApp/screen/panel/SqlEditorPanel.java
+++ src/main/java/tech/nmhillusion/jParrotDataSelectorApp/screen/panel/SqlEditorPanel.java
@@ -1,11 +1,16 @@
 package tech.nmhillusion.jParrotDataSelectorApp.screen.panel;
 
+import tech.nmhillusion.jParrotDataSelectorApp.helper.PathHelper;
 import tech.nmhillusion.jParrotDataSelectorApp.state.ExecutionState;
+import tech.nmhillusion.n2mix.helper.YamlReader;
 import tech.nmhillusion.neon_di.annotation.Inject;
 import tech.nmhillusion.neon_di.annotation.Neon;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 /**
  * created by: nmhillusion
@@ -15,13 +20,24 @@ import java.awt.*;
 @Neon
 public class SqlEditorPanel extends JPanel {
     private final ExecutionState executionState;
+    private final SqlEditorProperty sqlEditorProperty;
     private JTextArea sqlTextArea;
 
-    public SqlEditorPanel(@Inject ExecutionState executionState) {
+    public SqlEditorPanel(@Inject ExecutionState executionState) throws IOException {
         this.executionState = executionState;
-        setLayout(new BorderLayout());
+        sqlEditorProperty = new SqlEditorProperty(
+                getAppConfig("testing.enable", boolean.class)
+                , getAppConfig("testing.defaultSql", String.class)
+        );
 
+        setLayout(new BorderLayout());
         initComponents();
+    }
+
+    private <T> T getAppConfig(String configKey, Class<T> class2Cast) throws IOException {
+        try (final InputStream configStream = Files.newInputStream(PathHelper.getPathOfResource("config/app.config.yml"))) {
+            return new YamlReader(configStream).getProperty(configKey, class2Cast);
+        }
     }
 
     private void setHeightForComponent(Component comp, int height) {
@@ -40,9 +56,11 @@ public class SqlEditorPanel extends JPanel {
         this.sqlTextArea = new JTextArea();
         sqlTextArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
         //-- Mark: TEST: default SQL to test
-        sqlTextArea.setText(
-                "SELECT * FROM t_user;\nSELECT * FROM t_user tu \nwhere tu.enabled = 1\norder by tu.username\n;"
-        );
+        if (sqlEditorProperty.testingEnable()) {
+            sqlTextArea.setText(
+                    sqlEditorProperty.defaultSql()
+            );
+        }
         sqlTextArea.setLineWrap(false);
         sqlTextArea.setRows(18);
 
---
+++ src/main/java/tech/nmhillusion/jParrotDataSelectorApp/screen/panel/SqlEditorProperty.java
@@ -0,0 +1,9 @@
+package tech.nmhillusion.jParrotDataSelectorApp.screen.panel;
+
+/**
+ * created by: nmhillusion
+ * <p>
+ * created date: 2025-05-29
+ */
+public record SqlEditorProperty(boolean testingEnable, String defaultSql) {
+}
